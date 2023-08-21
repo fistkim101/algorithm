90842d90c66fcef869fd556e5ac2e4e870eb57e7
fistkim <fistkim101@gmail.com>
2023-08-21 23:16:24 +0900

    feat : 대소문자 변환

--- src/string/StringMain2.java
+++ src/string/StringMain2.java
@@ -1,7 +1,23 @@
 package string;
 
-public class StringMain2 {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class StringMain2 {
+    public String solution(String string){
+        StringBuilder result = new StringBuilder();
+        for(char character : string.toCharArray()){
+            if(Character.isLowerCase(character)){
+                result.append(String.valueOf(character).toUpperCase());
+            }else {
+                result.append(String.valueOf(character).toLowerCase());
+            }
+        }
+        return result.toString();
+    }
+    public static void main(String[] args) {
+        StringMain2 T = new StringMain2();
+        Scanner sc = new Scanner(System.in);
+        String string = sc.next();
+        System.out.println(T.solution(string));
     }
 }
