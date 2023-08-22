d4b4b3b1ddeb977a5dbb306b0492804591296aba
fistkim <fistkim101@gmail.com>
2023-08-22 11:25:42 +0900

    feat : 중복 문자 제거

---
+++ src/string/StringMain6.java
@@ -0,0 +1,39 @@
+package string;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class StringMain6 {
+
+    public void solution(String str) {
+
+        StringBuilder stringBuilder = new StringBuilder();
+        for(int i = 0; i < str.length(); i++){
+            int originalIndex= i;
+            int firstIndex = str.indexOf(str.charAt(i));
+            if(originalIndex == firstIndex){
+                stringBuilder.append(str.charAt(i));
+            }
+        }
+
+        System.out.println(stringBuilder);
+
+//        Set<Character> charsSet = new LinkedHashSet<>();
+//        char[] chars = str.toCharArray();
+//        for (char c : chars) {
+//            charsSet.add(c);
+//        }
+//        StringBuilder stringBuilder = new StringBuilder();
+//        charsSet.forEach(stringBuilder::append);
+//        System.out.println(stringBuilder);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.next();
+        StringMain6 T = new StringMain6();
+        T.solution(str);
+    }
+}
