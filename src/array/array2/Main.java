bdbacca7bbfc0ebe2dee620811a2913c8bedee61
fistkim101 <fistkim101@gmail.com>
2023-09-01 14:00:12 +0900

    feat : 보이는 학생

--- src/array/array2/Main.java
+++ src/array/array2/Main.java
@@ -4,13 +4,24 @@ import java.util.Scanner;
 
 public class Main {
 
-    public void solution(){
-
-    }
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int count = scanner.nextInt();
+        int[] array = new int[count];
+        for (int i = 0; i < count; i++) {
+            array[i] = scanner.nextInt();
+        }
 
+        int max = 0;
+        int result = 0;
+        for (int i : array) {
+            if (i > max) {
+                result++;
+                max = i;
+            }
+        }
+
+        System.out.println(result);
     }
 
 }
