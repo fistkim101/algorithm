208016bea48ffcd3f4e8e10e45edd94344d99231
fistkim101 <fistkim101@gmail.com>
2023-09-30 12:14:23 +0900

    feat : ~ 정렬

--- src/programers/array/solution3/Solution.java
+++ src/programers/array/solution3/Solution.java
@@ -1,4 +1,27 @@
 package programers.array.solution3;
 
-public class Solution {
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] citations) {
+        Arrays.sort(citations);
+        int max = 0;
+        for (int i = 0; i < citations.length; i++) {
+            int biggerSize = citations.length - i;
+            int current = citations[i];
+            if (current >= biggerSize) {
+                max = Math.max(max, biggerSize);
+            }
+        }
+
+        return max;
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Solution T = new Solution();
+        int answer = T.solution(new int[]{3, 0, 6, 1, 5}); // 3
+        System.out.println(answer);
+    }
 }
